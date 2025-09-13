0df40ed556124be2580c11925990563f711cac9c
khoidauminh <casboi86@gmail.com>
2025-09-13 09:30:41 +0700

    Logging: Move attribute to header file

--- src/util/logging.c
+++ src/util/logging.c
@@ -45,7 +45,7 @@ void error(const char *msg, ...) {
     fprintf(stderr, "\x1B[0m");
 }
 
-[[noreturn]] void die(const char *msg, ...) {
+void die(const char *msg, ...) {
     fprintf(stderr, "\x1B[31;1m");
     fprintf(stderr, "!!! ERROR: ");
 
